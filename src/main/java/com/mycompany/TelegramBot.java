eb3449d8c70be40baa4bec819a9d49e14de020c7
Denis Styopshin <d.stepshin@andersenlab.eu>
2025-09-23 06:21:09 +0500

    refactor: adjust tg bot code to user classes changes

--- src/main/java/com/mycompany/TelegramBot.java
+++ src/main/java/com/mycompany/TelegramBot.java
@@ -1,12 +1,12 @@
 package com.mycompany;
 
-import com.mycompany.currency.*;
-import com.mycompany.entity.User;
+import com.mycompany.currency.CurrencyPair;
 import com.mycompany.currency.CurrencyQuotes;
+import com.mycompany.dto.UserDto;
+import com.mycompany.entity.User;
+import com.mycompany.mapper.UserMapper;
 import com.mycompany.my.MyTimer;
 import com.mycompany.service.UserService;
-import com.mycompany.service.UserServiceImpl;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -24,32 +24,31 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
-import static com.mycompany.currency.BybitCryptocurrencyPair.*;
-import static com.mycompany.currency.CalculatedQuoteCurrencyPair.*;
-import static com.mycompany.currency.MoexCurrencyPair.*;
-
-/**
- * Класс, описывающий Telegram-бота.
- */
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
   private final String username;
   private final String token;
   private final UserService userService;
+  private final UserMapper mapper;
 
   @Autowired
   public TelegramBot(
       @Value("${bot.username}") String username,
       @Value("${bot.token}") String token,
-      UserService userService) {
+      UserService userService,
+      UserMapper mapper) {
+    super(token);
     this.username = username;
     this.token = token;
     this.userService = userService;
+    this.mapper = mapper;
   }
 
   @Override
@@ -57,41 +56,9 @@ public class TelegramBot extends TelegramLongPollingBot {
     return username; // этот параметр можно получить у телеграм-бота @BotFather https://t.me/BotFather
   }
 
-  @Override
-  public String getBotToken() {
-    return token; // этот параметр можно получить у телеграм-бота @BotFather https://t.me/BotFather
-  }
-
-
-  // котировки валютных пар
   private CurrencyQuotes quotes = new CurrencyQuotes();
-
-  // Коллекция для хранения множества chatId пользователей бота
-  private Set<Long> userChatIds = ConcurrentHashMap.newKeySet(); // так мы получаем потокобезопасный HashSet
-
-
-  private User user = readUserFromDataBase();
-  private User temp = new User(user);
-  private User tempBefore;
-
-  // имитируем чтение инфы из базы
-  private User readUserFromDataBase() {
-    Map<String, Boolean> settings = new LinkedHashMap<>();
-    settings.put(USD_RUB.name(), false);
-    settings.put(EUR_RUB.name(), false);
-    settings.put(CNY_RUB.name(), false);
-    settings.put(TRY_RUB.name(), false);
-    settings.put(EUR_USD.name(), false);
-    settings.put(USD_KZT.name(), false);
-    settings.put(RUB_KZT.name(), false);
-    settings.put(USD_BYN.name(), false);
-    settings.put(BTC_USDT.name(), true);
-    settings.put(ETH_USDT.name(), true);
-    settings.put(SOL_USDT.name(), false);
-    settings.put(WLKN_USDT.name(), true);
-
-    return new User(3, "den", "chik", "pyramid", settings);
-  }
+  private User user;
+  private UserDto tempUser;
 
   // текстовые константы для callback data кнопок меню "настройки"
   private static final String FIAT_CURRENCIES_SETTINGS = "fiatSettings";
@@ -118,13 +85,8 @@ public class TelegramBot extends TelegramLongPollingBot {
             """;
   }
 
-  //    Ссылка на сообщение с клавиатурой, которое нужно будет редактировать или удалять.
-//
-//    Эта ссылка должна быть именно полем класса, а не локальной переменной метода onUpdateReceived(), т.к. каждое
-//    новое нажатие любой из кнопок - это новый вызов этого метода. Если эта переменная будет локальной переменной
-//    метода, ее значение будет обнуляться после каждого нажатия кнопок. А нам нужно, чтобы состояние этой переменной
-//    сохранялось от одного нажатия кнопки к другому.
-  private Message messageWithKeyboard;
+  // Ссылка на сообщение с клавиатурой, которое нужно будет редактировать или удалять.
+  private Message messageWithKeyboard; // должна быть именно полем класса, а не локальной переменной
 
   // TODO https://javarush.com/groups/posts/3219-java-proekt-ot-a-do-ja-realizuem-command-pattern-dlja-rabotih-s-botom-chastjh-1
   // вызывается автоматически всякий раз при получении сообщения (update) от юзера
@@ -138,13 +100,12 @@ public class TelegramBot extends TelegramLongPollingBot {
       // обработка нажатий пунктов меню
       switch (messageText) {
         case "/start" -> {
-          if (!userExists(chatId)) {
-            addUser(chatId); // ???????? нужна ли эта строка ?
+          if (!userService.exists(chatId)) {
 
             String firstName = update.getMessage().getFrom().getFirstName();
             String lastName = update.getMessage().getFrom().getLastName();
             String userName = update.getMessage().getFrom().getUserName();
-            userService.add(new User(chatId, firstName, lastName, userName));
+            userService.save(new User(chatId, firstName, lastName, userName));
           }
 
           // отправляем сообщения с котировками фиатных валют и крипты пользователю
@@ -154,14 +115,11 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
 
         case "/settings" -> {
-          // этот кусок кода при вызове настроек пользователем удаляет предыдущее сообщение с настройками
-          // таким образом мы не допускаем, чтобы в боте было одновременно два сообщения с настройками
-          // иначе - ошибки
           // удаляем предыдущее сообщение с настройками, если оно есть
           // сообщений с настройками должно быть только в единственном виде во избежание ошибок, связанных с этим дублированием
           if (messageWithKeyboard != null) {
             deletePreviousMessageWithKeyboard(chatId);
-            temp = new User(user); // также сбрасываем temp в исходное состояние
+            tempUser = mapper.toDto(user); // также сбрасываем temp в исходное состояние
           }
 
           var keyboard = createSettingsKeyboard();
@@ -170,126 +128,98 @@ public class TelegramBot extends TelegramLongPollingBot {
 
         case "/exit" -> {
           sendMessage(chatId, "Бот остановлен !");
-          deleteUser(chatId);
           userService.delete(new User(chatId));
         }
       }
     } else if (update.hasCallbackQuery()) {
       var callbackQuery = update.getCallbackQuery();
       var callbackQueryData = callbackQuery.getData();
-      long chatId = callbackQuery.getMessage().getChatId();
+      long userId = callbackQuery.getMessage().getChatId();
 
-      // обработка нажатия кнопок
+      // обработка нажатия кнопок меню "Настройки"
       switch (callbackQueryData) {
         case FIAT_CURRENCIES_SETTINGS -> { // кнопка "отображение фиатных валют"
-          // удалить предыдущую клавиатуру
-          deletePreviousMessageWithKeyboard(chatId);
-          // не надо ли обнулять здесь messageWithKeyboard
-          // конкретно здесь необязательно, так как спустя пару строчек мы присваеваем в эту переменную новое значение
-
-          ///////////////////////////////////////////////////////////////////////////
-          // сначала надо считать настройки из базы и применить их настройках бота //
-          ///////////////////////////////////////////////////////////////////////////
-
-          var keyboard = createFiatCurrenciesKeyboard(user); // user или temp???
-          messageWithKeyboard = sendMessage(chatId, FIAT_SETTINGS_MESSAGE_TEXT, keyboard);
+          deletePreviousMessageWithKeyboard(userId); // удалить предыдущую клавиатуру
+          user = userService.getById(userId);
+          tempUser = mapper.toDto(user);
+          var keyboard = createFiatCurrenciesKeyboard(mapper.toDto(user)); // user или temp???
+          messageWithKeyboard = sendMessage(userId, FIAT_SETTINGS_MESSAGE_TEXT, keyboard);
         }
 
         case CRYPTO_CURRENCIES_SETTINGS -> { // кнопка "отображение криптовалют"
-          deletePreviousMessageWithKeyboard(chatId);
-
-          ///////////////////////////////////////////////////////////////////////////
-          // сначала надо считать настройки из базы и применить их настройках бота //
-          ///////////////////////////////////////////////////////////////////////////
-
-          var keyboard = createCryptoCurrenciesKeyboard(user);
-          messageWithKeyboard = sendMessage(chatId, CRYPTO_SETTINGS_MESSAGE_TEXT, keyboard);
+          deletePreviousMessageWithKeyboard(userId);
+          user = userService.getById(userId);
+          var keyboard = createCryptoCurrenciesKeyboard(mapper.toDto(user));
+          messageWithKeyboard = sendMessage(userId, CRYPTO_SETTINGS_MESSAGE_TEXT, keyboard);
         }
 
-        case EXIT -> { // кнопка "Выход"
-          deletePreviousMessageWithKeyboard(chatId);
-        }
+        case EXIT -> deletePreviousMessageWithKeyboard(userId); // кнопка "Выход"
 
         case SHOW_ALL_FIAT -> { // кнопка "показывать все" в меню "отображение фиатных валют"
-          temp.enableFiat(); //user.enableFiat();
-          InlineKeyboardMarkup keyboard = createFiatCurrenciesKeyboard(temp); // читаем из user
-          editMessage(chatId, messageWithKeyboard.getMessageId(), keyboard);
+          tempUser.enableFiat();
+          InlineKeyboardMarkup keyboard = createFiatCurrenciesKeyboard(tempUser);
+          editMessage(userId, messageWithKeyboard.getMessageId(), keyboard);
         }
 
-        case
-            DONT_SHOW_ALL_FIAT -> { // кнопка "не показывать все" в меню "отображение фиатных валют"
-          temp.disableFiat(); //user.disableFiat();
-          InlineKeyboardMarkup keyboard = createFiatCurrenciesKeyboard(temp);
-          editMessage(chatId, messageWithKeyboard.getMessageId(),
-              keyboard); // error: message is not modified
+        case DONT_SHOW_ALL_FIAT -> { //кнопка "не показывать все" в меню "отображение фиатных валют"
+          tempUser.disableFiat();
+          InlineKeyboardMarkup keyboard = createFiatCurrenciesKeyboard(tempUser);
+          editMessage(userId, messageWithKeyboard.getMessageId(), keyboard);
         }
 
         case SHOW_ALL_CRYPTO -> { // кнопка "показывать все" в меню "отображение криптовалют"
-          temp.enableCrypto(); //user.enableCrypto();
-          InlineKeyboardMarkup keyboard = createCryptoCurrenciesKeyboard(temp);
-          editMessage(chatId, messageWithKeyboard.getMessageId(),
+          tempUser.enableCrypto();
+          InlineKeyboardMarkup keyboard = createCryptoCurrenciesKeyboard(tempUser);
+          editMessage(userId, messageWithKeyboard.getMessageId(),
               keyboard);  // error: message is not modified
         }
 
-        case
-            DONT_SHOW_ALL_CRYPTO -> { // кнопка "не показывать все" в меню "отображение криптовалют"
-          temp.disableCrypto(); //user.disableCrypto();
-          InlineKeyboardMarkup keyboard = createCryptoCurrenciesKeyboard(temp);
-          editMessage(chatId, messageWithKeyboard.getMessageId(), keyboard);
+        case DONT_SHOW_ALL_CRYPTO -> { // кнопка "не показывать все" в меню "отображение криптовалют"
+          tempUser = mapper.toDto(user);
+          tempUser.disableCrypto();
+          InlineKeyboardMarkup keyboard = createCryptoCurrenciesKeyboard(tempUser);
+          editMessage(userId, messageWithKeyboard.getMessageId(), keyboard);
         }
 
         case SAVE -> { // кнопка "Сохранить"
-          deletePreviousMessageWithKeyboard(chatId);
-
-          ///////////////////////////////////////////////
-          // здесь д.б. код сохранения настроек в базе //
-          ///////////////////////////////////////////////
-
-          user = new User(temp);
-          sendMessage(chatId, "Настройки сохранены!");
+          deletePreviousMessageWithKeyboard(userId);
+          user.getDisplayableCurrencies().clear();
+          user.getDisplayableCurrencies().addAll(tempUser.getDisplayableCurrencies());
+          userService.save(user);
+          user = mapper.toEntity(tempUser);
+          sendMessage(userId, "Настройки сохранены!");
         }
 
         case BACK -> { // кнопка "< Назад"
-          temp = new User(user); //////////////////////////////////////////////
-          deletePreviousMessageWithKeyboard(chatId);
+          tempUser = mapper.toDto(user);
+          deletePreviousMessageWithKeyboard(userId);
           var keyboard = createSettingsKeyboard();
-          messageWithKeyboard = sendMessage(chatId, "Что необходимо настроить ?", keyboard);
+          messageWithKeyboard = sendMessage(userId, "Что необходимо настроить ?", keyboard);
         }
 
         default -> { // эта ветка обрабатывает нажатия кнопок с валютными парами
-          if (temp.getUserSettings().containsKey(callbackQueryData)) { // если это валютная пара
-            //if (user.getUserSettings().containsKey(callbackQueryData)) { // если это валютная пара
-            //user.toggleShow(callbackQueryData); // инвертируем настройку показа этой валютной пары
-            temp.toggleShow(callbackQueryData); // инвертируем настройку показа этой валютной пары
+          Set<String> allCurrencyPairNames = CurrencyQuotes.ALL_CURRENCY_PAIRS.stream()
+              .map(CurrencyPair::name)
+              .collect(Collectors.toSet());
+
+          if (allCurrencyPairNames.contains(callbackQueryData)) { // если это валютная пара
+            var currencyPairName = callbackQueryData;
+            tempUser.toggleShow(currencyPairName); // инвертируем настройку показа этой валютной пары
             InlineKeyboardMarkup keyboard = null;
             // здесь я при нажатии на кнопку создаю новую клавиатуру и вставляю ее в editMessage. Мб имеет
             // смысл редактировать только одну кнопку, а остальную клаву не менять ?
-            if (CurrencyPair.valueOf(callbackQueryData).isFiat()) {
-              keyboard = createFiatCurrenciesKeyboard(temp);
+            if (CurrencyPair.valueOf(currencyPairName).isFiat()) {
+              keyboard = createFiatCurrenciesKeyboard(tempUser);
             }
 
-            if (CurrencyPair.valueOf(callbackQueryData).isCrypto()) {
-              keyboard = createCryptoCurrenciesKeyboard(temp);
+            if (CurrencyPair.valueOf(currencyPairName).isCrypto()) {
+              keyboard = createCryptoCurrenciesKeyboard(tempUser);
             }
-            editMessage(chatId, messageWithKeyboard.getMessageId(), keyboard);
+            editMessage(userId, messageWithKeyboard.getMessageId(), keyboard);
           }
         }
       }
     }
-  }
-
-  // добавляет chatId нового юзера
-  public void addUser(long userChatId) {
-    userChatIds.add(userChatId);
-  }
-
-  // удаляет chatId юзера
-  public void deleteUser(long userChatId) {
-    userChatIds.remove(userChatId);
-  }
-
-  private boolean userExists(long chatId) {
-    return userService.getByChatId(chatId) != null;
   }
 
   /**
@@ -339,7 +269,8 @@ public class TelegramBot extends TelegramLongPollingBot {
   private void sendToAll(String text) {
     SendMessage sendMessage = new SendMessage();
     try {
-      for (long chatId : userChatIds) {
+      var allUserIds = userService.getAllUserIds();
+      for (long chatId : allUserIds) {
         sendMessage.setChatId(chatId); // почему я здесь не юзаю метод send() ????
         sendMessage.setText(text);
         execute(sendMessage); // отправляем сообщение
@@ -376,11 +307,8 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
   }
 
-  /**
-   *
-   */
   private void editMessage(long chatId, int messageId, InlineKeyboardMarkup keyboard) {
-    var tempBeforeTemp = new User(temp);
+    //var tempBeforeTemp = new User(temp);
     try {
       // сделать проверку: сообщение изменено или то же самое // мб сравнивать user и temp???????????????
       execute(EditMessageReplyMarkup
@@ -411,9 +339,6 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
   }
 
-  /**
-   *
-   */
   private void deleteMessage(long chatId, Message message) {
     try {
       execute(DeleteMessage.builder().chatId(chatId).messageId(message.getMessageId()).build());
@@ -440,18 +365,18 @@ public class TelegramBot extends TelegramLongPollingBot {
     return new InlineKeyboardMarkup(rows);
   }
 
-  private InlineKeyboardMarkup createFiatCurrenciesKeyboard(User user) {
+  private InlineKeyboardMarkup createFiatCurrenciesKeyboard(UserDto user) {
     return createCurrenciesKeyboard(CurrencyQuotes.FIAT_CURRENCY_PAIRS, user);
   }
 
   // клавиатура для настройки отображения крипты
-  private InlineKeyboardMarkup createCryptoCurrenciesKeyboard(User user) {
+  private InlineKeyboardMarkup createCryptoCurrenciesKeyboard(UserDto user) {
     return createCurrenciesKeyboard(Utilities.CRYPTO_CURRENCY_PAIRS, user);
   }
 
   // клавиатура для настройки отображения валютных пар
   private InlineKeyboardMarkup createCurrenciesKeyboard(List<CurrencyPair> currencyPairs,
-      User user) {
+      UserDto user) {
     // из списка валютных пар создаем строки с кнопками, в каждой по две кнопки
     List<List<InlineKeyboardButton>> keyboardRows = new ArrayList<>();
     for (int i = 0; i < currencyPairs.size(); i += 2) {
@@ -482,7 +407,7 @@ public class TelegramBot extends TelegramLongPollingBot {
 
   // создает кнопку с текстом и callbackData
   // callbackData - это инфа, которая будет отправлена на сервер при нажатии кнопки
-  private InlineKeyboardButton createCurrencyPairButton(CurrencyPair currencyPair, User user) {
+  private InlineKeyboardButton createCurrencyPairButton(CurrencyPair currencyPair, UserDto user) {
     String textOnButton = currencyPair.getShortName();
     if (user.isShow(currencyPair)) {
       textOnButton += ": ✅";
@@ -501,7 +426,7 @@ public class TelegramBot extends TelegramLongPollingBot {
   }
 
   private List<InlineKeyboardButton> createRowWithButtonsFor(CurrencyPair first,
-      CurrencyPair second, User user) {
+      CurrencyPair second, UserDto user) {
     return List.of(
         createCurrencyPairButton(first, user),
         createCurrencyPairButton(second, user)
