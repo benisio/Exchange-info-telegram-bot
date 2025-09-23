55126e34d389ade2b7a3d525ae01adb58a90722b
Denis Styopshin <d.stepshin@andersenlab.eu>
2025-09-23 06:19:24 +0500

    refactor: user model and entity

--- src/main/java/com/mycompany/entity/User.java
+++ src/main/java/com/mycompany/entity/User.java
@@ -1,17 +1,10 @@
 package com.mycompany.entity;
 
-import com.mycompany.Utilities;
-import com.mycompany.currency.CurrencyPair;
-import com.mycompany.currency.CurrencyQuotes;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.Cascade;
 
 import java.time.Instant;
-import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import static com.mycompany.currency.BybitCryptocurrencyPair.*;
@@ -30,7 +23,7 @@ public class User {
 
   @Id
   @Column(name = "telegram_user_id")
-  private long chatId;
+  private long id;
 
   @Column(name = "first_name")
   private String firstName;
@@ -52,21 +45,6 @@ public class User {
   @Column(name = "currency_pair", nullable = false)
   private Set<String> displayableCurrencies = new LinkedHashSet<>();
 
-    /*@ElementCollection(fetch = FetchType.EAGER)
-    @Cascade({ org.hibernate.annotations.CascadeType.ALL }) // обозначает, какие из методов интерфейса Session будут распространяться каскадно к ассоциированным сущностям
-    @CollectionTable(
-            name = "user_settings", // имя вспомогательной таблицы
-            joinColumns = @JoinColumn( // для задания ссылки на внешний ключ
-                                        name="user_chat_id", // название внешнего ключа
-                                        foreignKey = @ForeignKey(
-                                                name = "user_settings_fk"*//*, // название внешнего ключа
-                                                value = ConstraintMode.CONSTRAINT,
-                                                foreignKeyDefinition = "foreign key (user_chat_id) references" +
-                                                        "telegram_users ON DELETE CASCADE"*//*)))
-    @MapKeyColumn(name = "currency_pair") // название столбца для ключей
-    @Column(name = "is_show") // название столбца для значений
-    private Map<String, Boolean> displayableCurrencies = new LinkedHashMap<>();*/
-
   private static final Set<String> DEFAULT_SETTINGS;
 
   static {
@@ -87,62 +65,12 @@ public class User {
     DEFAULT_SETTINGS.add(LINEA_USDT.name());
   }
 
-  public User(long chatId) {
-    this.chatId = chatId;
+  public User(long id) {
+    this.id = id;
   }
 
-  public User(long chatId, String firstName, String lastName, String userName) {
-    this(chatId, firstName, lastName, userName, Instant.now(), DEFAULT_SETTINGS);
-  }
-
-  // конструктор клонирования
-  public User(User userToClone) {
-    this.chatId = userToClone.chatId;
-    this.firstName = userToClone.firstName;
-    this.lastName = userToClone.lastName;
-    this.userName = userToClone.userName;
-    this.registeredAt = userToClone.registeredAt;
-    this.displayableCurrencies = new LinkedHashSet<>(userToClone.displayableCurrencies);
-  }
-
-  public User(long chatId, String firstName, String lastName, String userName,
-      Set<String> settings) {
-    this(chatId, firstName, lastName, userName, Instant.now(), settings);
-  }
-
-  public void toggleShow(String currencyPairName) {
-    boolean isShow = displayableCurrencies.contains(currencyPairName);
-    displayableCurrencies.add(currencyPairName);
-  }
-
-  public boolean isShow(CurrencyPair currencyPair) {
-    return displayableCurrencies.contains(currencyPair.name());
-  }
-
-  public void enableFiat() {
-    show(CurrencyQuotes.FIAT_CURRENCY_PAIRS, true);
-  }
-
-  public void disableFiat() {
-    show(CurrencyQuotes.FIAT_CURRENCY_PAIRS, false);
-  }
-
-  public void enableCrypto() {
-    show(Utilities.CRYPTO_CURRENCY_PAIRS, true);
-  }
-
-  public void disableCrypto() {
-    show(Utilities.CRYPTO_CURRENCY_PAIRS, false);
-  }
-
-  private void show(List<CurrencyPair> currencyPairs, boolean show) {
-    for (CurrencyPair pair : currencyPairs) {
-      if (show) {
-        displayableCurrencies.add(pair.name());
-      } else {
-        displayableCurrencies.remove(pair.name());
-      }
-    }
+  public User(long id, String firstName, String lastName, String userName) {
+    this(id, firstName, lastName, userName, Instant.now(), DEFAULT_SETTINGS);
   }
 
 }
---
+++ src/main/java/com/mycompany/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.mycompany.mapper;
+
+import com.mycompany.dto.UserDto;
+import com.mycompany.entity.User;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring") // или "default", если без Spring
+public interface UserMapper {
+
+  UserDto toDto(User entity);
+
+  User toEntity(UserDto dto);
+
+  List<UserDto> toDtoList(List<User> entities);
+  List<User> toEntityList(List<UserDto> dtos);
+}
